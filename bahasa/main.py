0d0d27c084274f9b030544c76df8fa7f5bac59f7
Goutham Krishna K V <gauthamkrishna9991@live.com>
2023-04-08 20:24:41 +0530

    feat: Create a video capture device (preliminary)

--- bahasa/main.py
+++ bahasa/main.py
@@ -1,25 +1,45 @@
 from cv2 import VideoCapture
 import cv2
+import numpy as np
+
+class VCDevice:
+    vcIndex: int
+    vcDevice: VideoCapture
+
+    def __init__(self, vcIndex: int = 0):
+        self.vcIndex = vcIndex
+        self.vcDevice = cv2.VideoCapture(0)
+        if not self.vcDevice.isOpened():
+            raise OSError(1, "Error in finding camera")
+
+    def run_pipeline(self, frame: np.array) -> np.array:
+        # Image processing pipeline goes here
+        return frame
+    
+    def run(self):
+        isErrored, errorVal = False, None
+        while True:
+            # Read a frame
+            ret, frame = self.vcDevice.read()
+            # Check if frame exists
+            if not ret:
+                isErrored, errorVal = True, "Can't recieve from camera!"
+            # Process the frame from the pipeline
+            processed_frame = self.run_pipeline(frame)
+            # Show the frame
+            cv2.imshow('frame', processed_frame)
+            # Wait 1ms for a key clicked
+            if cv2.waitKey(1) == ord('q'):
+                break
+        self.vcDevice.release()
+        cv2.destroyAllWindows()
+
+        if isErrored:
+            raise OSError(1, errorVal)
+
 
 def main():
-    vcap: VideoCapture = cv2.VideoCapture(0)
-
-    if not vcap.isOpened():
-        print("Error in finding camera")
-        exit(1)
-    
-    print("Click `q` in the window to close it.")
-    
-    while True:
-        ret, frame = vcap.read()
-        if not ret:
-            print(f"Can't recieve from camera (stream end?). Exiting")
-            exit(1)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # Display the grayscale
-        cv2.imshow('frame', gray)
-        if cv2.waitKey(1) == ord('q'):
-            break
-    
-    vcap.release()
-    cv2.destroyAllWindows()
+    # Create the video capture device
+    dev = VCDevice()
+    # Run the device
+    dev.run()
